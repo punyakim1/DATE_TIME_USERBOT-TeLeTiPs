c9955d73866899840087cbc1c50c3e4645cff252
punyakim1 <126011201+punyakim1@users.noreply.github.com>
2023-04-07 04:33:30 +0700

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -40,7 +40,7 @@ async def main_teletips():
                 Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
                 Image_teletips.save("Image_final_teletips.jpg")
                 await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"Bunda ren | ⏰{Time_teletips}")
-                #await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
+                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = Date_Time_Userbot_teletips.get_chat_photos("me")
                 try:
